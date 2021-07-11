5fcb568c53b0541f7ca77352f6cbba6f73bc0d7f
Unknown <ascalon0823@gmail.com>
2021-07-11 11:36:08 +0800

    REFACTOR A BIT

--- Assets/Arkademy/Game/MapBuilder.cs
+++ Assets/Arkademy/Game/MapBuilder.cs
@@ -8,8 +8,9 @@ namespace Arkademy.Game
         [SerializeField] private int seed;
         [SerializeField] private bool generateNewSeed;
         [Range(0, 1)] [SerializeField] private float walkablePercent;
-
+        [Header("Perlin setup")]
         [Range(1, 32)] [SerializeField] private float frequency;
+        [Range(1, 10000)][SerializeField] private int offSetRange;
 
         public void BuildMap(Map map)
         {
@@ -19,16 +20,30 @@ namespace Arkademy.Game
             }
 
             Random.InitState(seed);
+            
+            var noiseMap = CreatePerlinNoiseMap(map.Width(),map.Height());
             var percent = walkablePercent;
-            var freq = frequency;
-            var org = (Random.insideUnitCircle+Vector2.one)/2*Random.Range(0,10000);
             map.Iterate((i, j) =>
             {
                 var mapCell = map[i, j];
-                mapCell.walkable = Mathf.PerlinNoise( org.x + 1.0f * i / map.Width() * freq
-                    ,  org.y + 1.0f * j / map.Height() * freq) < percent;
+                mapCell.walkable = noiseMap[i,j]< percent;
                 map[i, j] = mapCell;
             });
+        }
+
+        float[,] CreatePerlinNoiseMap(int x, int y)
+        {
+            var result = new float[x, y];
+            var org = (Random.insideUnitCircle+Vector2.one)/2*offSetRange;
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    result[i,j] = Mathf.PerlinNoise(org.x + 1.0f * i / x * frequency
+                        , org.y + 1.0f * j / y * frequency);
+                }
+            }
+            return result;
         }
     }
 }
