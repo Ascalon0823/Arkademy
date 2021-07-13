e4677139347e059f20930e0dfb947bfb1bf22572
Ascalon <Ascalon0823@Gmail.com>
2021-07-13 13:34:05 +0800

    Rename

--- Assets/Arkademy/GenericGrid.cs
+++ Assets/Arkademy/GenericGrid.cs
@@ -2,56 +2,56 @@ namespace Arkademy
 {
     public class Grid<T>
     {
-        private readonly T[,] data;
+        private readonly T[,] _data;
         
         public Grid(int x, int y)
         {
-            data = new T[x, y];
+            _data = new T[x, y];
         }
 
         public Grid(int x, int y, T val)
         {
-            data = new T[x, y];
+            _data = new T[x, y];
             Fill(val);
         }
 
         public Grid(T[,] d)
         {
-            data = d;
+            _data = d;
         }
 
         public int Width()
         {
-            return data.GetLength(0);
+            return _data.GetLength(0);
         }
 
         public int Height()
         {
-            return data.GetLength(1);
+            return _data.GetLength(1);
         }
         
         public T this[int x,int y]
         {
-            get => data[x, y];
-            set => data[x, y] = value;
+            get => _data[x, y];
+            set => _data[x, y] = value;
         }
 
 
         public void Fill(T val)
         {
-            Iterate((i, j) => data[i, j] = val);
+            Iterate((i, j) => _data[i, j] = val);
         }
         
         public void Iterate(System.Action<int, int> action)
         {
-            if (data == null)
+            if (_data == null)
             {
                 return;
             }
 
-            for (var j = 0; j < data.GetLength(1); j++)
+            for (var j = 0; j < _data.GetLength(1); j++)
             {
-                for (var i = 0; i < data.GetLength(0); i++)
+                for (var i = 0; i < _data.GetLength(0); i++)
                 {
                     action?.Invoke(i, j);
                 }
@@ -59,18 +59,18 @@ namespace Arkademy
         }
         public override string ToString()
         {
-            if (data == null)
+            if (_data == null)
             {
                 return "null";
             }
 
             var print = "";
-            for (var j = 0; j < data.GetLength(1); j++)
+            for (var j = 0; j < _data.GetLength(1); j++)
             {
                 print += "[";
-                for (var i = 0; i < data.GetLength(0); i++)
+                for (var i = 0; i < _data.GetLength(0); i++)
                 {
-                    print += $" {data[i, j].ToString()} ";
+                    print += $" {_data[i, j].ToString()} ";
                 }
 
                 print += "]\n";
