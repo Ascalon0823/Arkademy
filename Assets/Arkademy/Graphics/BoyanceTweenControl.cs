59c6599309ec6b6f1755c208e9e50d44e6a9ccf2
Ascalon <ascalon0823@gmail.com>
2022-10-22 15:32:46 +0800

    set pick up animation

--- Assets/Arkademy/Graphics/BoyanceTweenControl.cs
+++ Assets/Arkademy/Graphics/BoyanceTweenControl.cs
@@ -7,14 +7,21 @@ namespace Arkademy.Graphics
     {
         [SerializeField] private GameObject target;
 
+        [SerializeField] private float dropDistance;
+        [SerializeField] private float dropTime;
         [SerializeField] private float distance;
 
         [SerializeField] private float time;
 
         private void Start()
         {
-            LeanTween.moveLocalY(target, target.transform.localPosition.y + distance, time).setEaseInOutSine()
-                .setLoopPingPong();
+            LeanTween.moveLocalY(target, target.transform.localPosition.y + dropDistance, dropTime/2f).setEaseOutQuad()
+                .setLoopPingPong(1).setOnComplete(_ =>
+                {
+                    LeanTween.moveLocalY(target, target.transform.localPosition.y + distance, time).setEaseInOutSine()
+                        .setLoopPingPong();
+                });
+            LeanTween.rotateZ(target, -1080f, dropTime);
         }
     }
 }
